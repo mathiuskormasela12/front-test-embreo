5bf1b385c2f33e16d10076bf3db41e7a719103a2
Mathius Kormasela <mathiuskormasela12@gmail.com>
2021-08-23 18:01:53 +0700

    done vendor dashboard

--- src/components/modal-dashboard/ModalDashboard.js
+++ src/components/modal-dashboard/ModalDashboard.js
@@ -1,72 +1,99 @@
 // ========== Modal Dashboard
 // import all components
-import React from 'react';
+import React, { Fragment } from 'react';
 
 // import bootstrap components
 import {
 	Modal,
 	Button,
 	Container,
-	Row,
-	Col,
-	ListGroup
+	Table
 } from 'react-bootstrap';
 
 export function ModalDashboard(props) {
-	const removeShadow = {
-		'border-left': 'none',
-		'border-right': 'none'
-	}
+	// const removeShadow = {
+	// 	borderTopLeft: 'none',
+	// 	borderTopRight: 'none',
+	// 	borderBottomLeft: 'none',
+	// 	borderBottomRight: 'none'
+	// }
 
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Event Details
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Container fluid>
-					<Row noGutters>
-						<Col lg={4}>
-							<ListGroup>
-								<ListGroup.Item>Event Name</ListGroup.Item>
-								<ListGroup.Item>Vendor Name</ListGroup.Item>
-								<ListGroup.Item>Confirmed Date</ListGroup.Item>
-								<ListGroup.Item>Status</ListGroup.Item>
-								<ListGroup.Item>Date Created</ListGroup.Item>
-							</ListGroup>
-						</Col>
-						<Col lg={1}>
-							<ListGroup>
-								<ListGroup.Item style={removeShadow}>:</ListGroup.Item>
-								<ListGroup.Item style={removeShadow}>:</ListGroup.Item>
-								<ListGroup.Item style={removeShadow}>:</ListGroup.Item>
-								<ListGroup.Item style={removeShadow}>:</ListGroup.Item>
-								<ListGroup.Item style={removeShadow}>:</ListGroup.Item>
-							</ListGroup>
-						</Col>
-						<Col lg={7}>
-							<ListGroup>
-								<ListGroup.Item>Launching Embreo</ListGroup.Item>
-								<ListGroup.Item>Embreo</ListGroup.Item>
-								<ListGroup.Item>14 September 2020</ListGroup.Item>
-								<ListGroup.Item>Approve</ListGroup.Item>
-								<ListGroup.Item>10 June 2020</ListGroup.Item>
-							</ListGroup>
-						</Col>
-					</Row>
-					{ props.children }
-				</Container>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
+  if(props.show) {
+		return (
+			<Modal
+				{...props}
+				size="lg"
+				aria-labelledby="contained-modal-title-vcenter"
+				centered
+			>
+				<Modal.Header closeButton>
+					<Modal.Title id="contained-modal-title-vcenter">
+						Event Details
+					</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Container>
+						<Table striped bordered hover size="md">
+							<thead>
+								<tr>
+									<th>Event Name</th>
+									<th>Vendor Name</th>
+									<th>Confirmed Date</th>
+									<th>Status</th>
+									<th>Date Created</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>{ props.detail.event_name }</td>
+									<td>{ props.detail.vendor_name }</td>
+									<td>
+										{ 
+											props.detail.date_event.some(item => item.status === 'approved') ? (
+												props.detail.date_event.filter(item => item.status === 'approved')[0].date
+											) : (
+												props.detail.date_event.some(item => item.status === 'rejected') ? (
+													props.detail.rejected_date
+												) : (
+													props.detail.date_event.map((item, index) => (
+														<Fragment key={String(index)}>
+															<p>{ item.date }</p>
+														</Fragment>
+													))
+												)
+											)
+										}
+									</td>
+									<td>
+										{ 
+											props.detail.date_event.some(item => item.status === 'approved') ? (
+												props.detail.date_event.filter(item => item.status === 'approved')[0].status
+											) : (
+												props.detail.date_event.some(item => item.status === 'rejected') ? (
+													props.detail.date_event.filter(item => item.status === 'rejected')[0].status
+												) : (
+													props.detail.date_event.map((item, index) => (
+														<Fragment key={String(index)}>
+															<p>{ item.status }</p>
+														</Fragment>
+													))
+												)
+											)
+										}
+									</td>
+									<td>{ props.detail.date_created }</td>
+								</tr>
+							</tbody>
+						</Table>
+						{ props.children }
+					</Container>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button onClick={props.onHide}>Close</Button>
+				</Modal.Footer>
+			</Modal>
+		);
+	} else {
+		return <Fragment></Fragment>
+	}
 }
